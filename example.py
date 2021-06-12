1c1f57276bce2a301cbc7a1e7568725939c3cf99
Cheese <adhesivecheese@gmail.com>
2021-06-11 21:33:36 -0400

    Simplify Golf

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from uuid import uuid4
 import praw
 from flask import Flask, request, url_for, abort
--- golfing_example.py
+++ golfing_example.py
@@ -2,17 +2,29 @@ import praw
 from praw.util.token_manager import BaseTokenManager
 from flask import Flask, request
 app = Flask(__name__)
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+REDIRECT_URI = ''
+USER_AGENT = 'minimal praw OAuth Webserver by u/'
+
 class customTokenManager(BaseTokenManager):
   token = ""
   def post_refresh_callback(self, authorizer): self.token = authorizer.refresh_token
   def pre_refresh_callback(self, authorizer): authorizer.refresh_token = self.token
   def set_initial_token(self, setToken): self.token = setToken
+
 refresh_token_manager = customTokenManager()
+
+
 @app.route('/')
 def homepage():	return f'<a href={auth_r.auth.url(["identity"], "state", "permanent")}>Permanent authorization link</a>'
+
+
 @app.route('/authorize_callback')
 def authorize_callback():
   refresh_token_manager.set_initial_token(auth_r.auth.authorize(request.args.get('code')))
   return f"You have {r.user.me().link_karma} link karma."
-auth_r = praw.Reddit(client_id='',client_secret='',redirect_uri='http://127.0.0.1:5000/authorize_callback',user_agent='minimal praw OAuth Webserver by u/')
-r = praw.Reddit(client_id='',client_secret='',token_manager=refresh_token_manager,user_agent='minimal praw OAuth Webserver by u/')
+
+
+auth_r = praw.Reddit(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri=REDIRECT_URI,user_agent=USER_AGENT)
+r = praw.Reddit(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,token_manager=refresh_token_manager,user_agent=USER_AGENT)
